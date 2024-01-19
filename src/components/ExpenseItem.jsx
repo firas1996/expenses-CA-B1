96df2af50be35c2d6fb517cc3d873255a7dfa689
firas hamila <firas.hm.1996@gmail.com>
2024-01-19 20:55:58 +0100

    nn

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpensesItem.css";
 
 const ExpenseItem = ({ title, date, price }) => {
+  const [newT, setNewT] = useState(title);
+  const updateTitle = () => {
+    console.log(newT);
+    setNewT("new title !!!!");
+    console.log(newT);
+  };
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newT}</h2>
+        <button onClick={updateTitle}>Update Title</button>
         <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
