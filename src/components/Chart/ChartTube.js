974e4e4baff74c3ec39101019621aa5c75f3b8ae
firas hamila <firas.hm.1996@gmail.com>
2024-01-19 19:20:19 +0100

    dd

---
+++ src/components/Chart/ChartTube.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ChartTube = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+      </div>
+      <div>label</div>
+    </div>
+  );
+};
+
+export default ChartTube;
