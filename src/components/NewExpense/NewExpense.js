9c6b7f520865d29d7e0e20e90ca98630eb154789
firas hamila <firas.hm.1996@gmail.com>
2024-02-16 19:25:52 +0100

    wxw

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,26 +1,90 @@
+import { useState } from "react";
 import "./NewExpense.css";
 
 const NewExpense = () => {
+  // const [title, setTitle] = useState("");
+  // const [price, setPrice] = useState("");
+  // const [date, setDate] = useState("");
+  const [data, setData] = useState({
+    title: "",
+    price: "",
+    date: "",
+  });
+  const titleChanger = (event) => {
+    // setTitle(event.target.value);
+    setData((prevState) => {
+      return { ...prevState, title: event.target.value };
+    });
+  };
+  const priceChanger = (event) => {
+    // setPrice(event.target.value);
+    setData({ ...data, price: event.target.value });
+  };
+  const dateChanger = (event) => {
+    // setDate(event.target.value);
+    setData({ ...data, date: event.target.value });
+  };
+  const handelAll = (event) => {
+    const { name, value } = event.target;
+    console.log(name);
+    setData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+    // console.log(event.target.name);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // console.log(title);
+    // console.log(price);
+    // console.log(date);
+    // setTitle("");
+    // setPrice("");
+    // setDate("");
+    console.log(data);
+  };
   return (
     <div className="new-expense">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input placeholder="Title" />
+            <input
+              required
+              placeholder="Title"
+              onChange={handelAll}
+              name="title"
+              value={data.title}
+            />
           </div>
           <div className="new-expense__control">
             <label>Price</label>
-            <input placeholder="Price" type="number" min="0" step="0.001" />
+            <input
+              required
+              placeholder="Price"
+              type="number"
+              min="0"
+              step="0.001"
+              onChange={handelAll}
+              value={data.price}
+              name="price"
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2022-01-01" max="2026-12-31" />
+            <input
+              required
+              type="date"
+              min="2022-01-01"
+              max="2026-12-31"
+              onChange={handelAll}
+              value={data.date}
+              name="date"
+            />
           </div>
         </div>
         <div className="new-expense__actions">
           <button>Cancel</button>
-          <button>Add Expense</button>
+          <button type="submit">Add Expense</button>
         </div>
       </form>
     </div>
